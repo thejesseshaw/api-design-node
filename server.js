5686133cf1d740693308561b9af050ff45473bf8
Jesse Shaw <jdshaw1987@gmail.com>
2017-09-18 13:03:58 -0400

    Initial Exercise

--- server.js
+++ server.js
@@ -2,4 +2,27 @@
 // that will send back the index.html file on a GET request to '/'
 // it should then send back jsonData on a GET to /data
 
+var express = require('express');
+var app = express();
+
 var jsonData = {count: 12, message: 'hey'};
+
+app.get('/todos', function(req, res) {
+    res.json(jsonData);
+});
+
+app,post('/todos', function(req, res) {
+    var todo = req.body.todo;
+    jsonData.push(jsonData);
+    res.send(todo);
+});
+
+app.get('/todos/:id', function(req, res) {
+    var todo = _.find(jsonData, {id: req.params.id});
+
+    res.json(todo);
+})
+
+app.listen(3000);
+
+
